3b12d5477c101a424d237f77dbc0cb322b9d3f0f
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-03-07 15:05:58 +0000

    Feels Like changes

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -4,39 +4,84 @@ import WeatherIcon from "./WeatherIcon.js";
 import WeatherUnits from "./WeatherUnits.js";
 
 export default function CurrentWeather(props) {
-  return (
-    <div className="WeatherAppBody">
-      <div className="TempToday">
-        <div className="row">
-          <div className="col-6">
-            <div className="float-left">
-              <WeatherIcon imag={props.info.icon} />
-            </div>
-            <ul>
-              <li>
-                <div className="text-capitalize">{props.info.description}</div>
-              </li>
-              <li>
-                <WeatherUnits
-                  celsius={props.info.temperature}
-                  celsiusFeelsLike={props.info.feelsLike}
-                  unit={props.unit}
-                  setUnit={props.setUnit}
-                />
-              </li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <div className="WeatherConditions">
+  if (props.unit === "celsius") {
+    return (
+      <div className="WeatherAppBody">
+        <div className="TempToday">
+          <div className="row">
+            <div className="col-6">
+              <div className="float-left">
+                <WeatherIcon imag={props.info.icon} />
+              </div>
               <ul>
-                <li>🌡Feels like: {Math.round(props.info.feelsLike)}º</li>
-                <li>🌢 Humidity: {Math.round(props.info.humidity)}%</li>
-                <li>💨Wind: {Math.round(props.info.wind)} km/h</li>
+                <li>
+                  <div className="text-capitalize">
+                    {props.info.description}
+                  </div>
+                </li>
+                <li>
+                  <WeatherUnits
+                    celsius={props.info.temperature}
+                    celsiusFeelsLike={props.info.feelsLike}
+                    unit={props.unit}
+                    setUnit={props.setUnit}
+                  />
+                </li>
               </ul>
+            </div>
+            <div className="col-6">
+              <div className="WeatherConditions">
+                <ul>
+                  <li>🌡Feels like: {Math.round(props.info.feelsLike)}º</li>
+                  <li>🌢 Humidity: {Math.round(props.info.humidity)}%</li>
+                  <li>💨Wind: {Math.round(props.info.wind)} km/h</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="WeatherAppBody">
+        <div className="TempToday">
+          <div className="row">
+            <div className="col-6">
+              <div className="float-left">
+                <WeatherIcon imag={props.info.icon} />
+              </div>
+              <ul>
+                <li>
+                  <div className="text-capitalize">
+                    {props.info.description}
+                  </div>
+                </li>
+                <li>
+                  <WeatherUnits
+                    celsius={props.info.temperature}
+                    celsiusFeelsLike={props.info.feelsLike}
+                    unit={props.unit}
+                    setUnit={props.setUnit}
+                  />
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <div className="WeatherConditions">
+                <ul>
+                  <li>
+                    🌡Feels like:{" "}
+                    {Math.round((props.info.feelsLike * 9) / 5 + 32)}º
+                  </li>
+                  <li>🌢 Humidity: {Math.round(props.info.humidity)}%</li>
+                  <li>💨Wind: {Math.round(props.info.wind)} km/h</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
